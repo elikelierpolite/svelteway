17b9c77a97dd1a914bc2ae1021960c006e0da364
Ndivhuwo Sithole <elikelierpolite@outlook.com>
2023-04-15 06:32:03 +0200

    no-prerender

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,22 +1,8 @@
 import type { LayoutServerLoad } from './$types';
-import { cwd } from 'process';
-import fs from 'node:fs';
 export const load = (async ({ route }) => {
-	const currentDirectory = cwd();
-	const fileToRead =
-		route.id == '/'
-			? `${currentDirectory}/src/routes/+page.svelte`
-			: `${currentDirectory}/src/routes/${route.id}/+page.svelte`;
-	const themeFile = `${currentDirectory}/static/theme.txt`;
-	const content = await fs.promises.readFile(fileToRead);
-	const themeContent = await fs.promises.readFile(themeFile);
 	return {
 		data: {
-			source: content.toString('utf8'),
-			file: fileToRead,
-			theme: JSON.stringify(themeContent.toString('utf8'))
+			theme: "light"
 		}
 	};
 }) satisfies LayoutServerLoad;
-
-export const prerender = true;
